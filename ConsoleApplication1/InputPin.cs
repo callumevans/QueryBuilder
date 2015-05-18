72168e814ab342555ec714a435c93da159f62805
Callum Evans <callumevans@outlook.com>
2015-05-18 19:13:28 +0100

    Initial commit

---
+++ ConsoleApplication1/InputPin.cs
@@ -0,0 +1,27 @@
+﻿using DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class InputPin : BaseNodePin
+    {
+        public OutputPin InputProviderPin { get; set; }
+
+        public IDataTypeContainer GetValue
+        {
+            get
+            {
+                return InputProviderPin.OutputValue;
+            }
+        }
+
+        public InputPin(Type inputType, GraphNode parent, OutputPin inputProvider = null) : base(inputType, parent)
+        {
+            InputProviderPin = inputProvider;
+        }
+    }
+}
