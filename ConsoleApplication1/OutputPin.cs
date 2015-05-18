d577f76ebf27787e46f06dd4a1b1c924de5e1b0d
Callum Evans <callumevans@outlook.com>
2015-05-18 19:10:58 +0100

    Initial commit

---
+++ ConsoleApplication1/OutputPin.cs
@@ -0,0 +1,22 @@
+﻿using DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class OutputPin : BaseNodePin
+    {
+        public bool OutputRealised { get; set; }
+        public List<InputPin> Connections { get; set; }
+        public IDataTypeContainer OutputValue { get; set; }
+
+        public OutputPin(Type outputType, GraphNode parent) : base(outputType, parent)
+        {
+            OutputRealised = false;
+            OutputValue = (IDataTypeContainer)Activator.CreateInstance(outputType);
+        }
+    }
+}
