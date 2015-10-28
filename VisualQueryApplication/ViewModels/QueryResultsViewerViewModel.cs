68376460405c6aa5876a15570005df7897736cda
Callum Evans <callumevans@outlook.com>
2015-10-28 14:00:50 +0000

    Git sync fix

---
+++ VisualQueryApplication/ViewModels/QueryResultsViewerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualQueryApplication.ViewModels
+{
+    public class QueryResultsViewerViewModel : ViewModelBase
+    {
+        public DataTable QueryResult
+        {
+            get { return queryResult; }
+            set
+            {
+                queryResult = value;
+                OnPropertyChanged(nameof(QueryResult));
+            }
+        }
+
+        private DataTable queryResult = new DataTable();
+
+        public QueryResultsViewerViewModel()
+        {
+        }
+
+        public QueryResultsViewerViewModel(string queryString)
+        {
+            if (App.CurrentDatabaseConnection == null)
+                throw new Exception("Establish a database connection first.");
+
+            SQLiteCommand query = new SQLiteCommand(queryString, App.CurrentDatabaseConnection);
+
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            QueryResult = dt;
+        }
+    }
+}
